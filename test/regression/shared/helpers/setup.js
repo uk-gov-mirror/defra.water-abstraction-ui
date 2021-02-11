2dae5e6f551e0303080a7d0e862bdaccebf55844
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2021-02-11 10:20:43 +0000

    bug/regression-tests -- method added to check water status

--- test/regression/shared/helpers/setup.js
+++ test/regression/shared/helpers/setup.js
@@ -15,17 +15,18 @@ const urlJoin = require('url-join');
  * @param {Object} [overrides]
  * @return {Promise}
  */
-const waterRequest = async (tail, overrides = {}) => {
+const waterRequest = async (tail, overrides = {}, method) => {
   const opts = Object.assign({}, {
     uri: urlJoin(process.env.WATER_URI, tail),
-    method: 'POST',
+    method,
     headers: {
       Authorization: `Bearer ${process.env.JWT_TOKEN}`
     }
   }, overrides);
   console.log(`Calling ${opts.method} ${opts.uri}`);
-  await rp(opts);
+  const data = await rp(opts);
   console.log(`Calling ${opts.method} ${opts.uri} - success!`);
+  console.log(data);
 };
 
 /**
@@ -36,13 +37,20 @@ const setUp = () => waterRequest('/acceptance-tests/set-up', {
   form: {
     includeInternalUsers: true
   }
-});
+}, 'POST');
+
+/**
+ * Sets up acceptance test data
+ * @return {Promise}
+ */
+const waterServerStatus = () => waterRequest('/service-status', {}, 'GET');
 
 /**
  * Tears down acceptance test data
  * @return {Promise}
  */
-const tearDown = () => waterRequest('/acceptance-tests/tear-down');
+const tearDown = () => waterRequest('/acceptance-tests/tear-down', {}, 'POST');
 
 exports.setUp = setUp;
 exports.tearDown = tearDown;
+exports.waterServerStatus = waterServerStatus;
